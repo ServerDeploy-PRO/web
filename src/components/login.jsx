8fd4525760435967ceb9b624b7e9d735d7010831
Hamza Elashry <98109755+arkseek@users.noreply.github.com>
2024-04-14 23:14:54 -0400

    Create login.jsx

---
+++ src/components/login.jsx
@@ -0,0 +1,18 @@
+export default function Login() {
+    return(
+        <>
+        
+        <div className="font-sans text-center border-2 mt-64 w-96 rounded-md">
+            <h1 className="text-2xl text-gray-200">ServerPay</h1>
+            <p className="text-gray-400">Havent signed up yet? Sign up <a href="/signup" className="text-sky-600">Here!</a></p>
+            <hr />
+            <p className="text-sky-600">Email</p>
+            <input type="search" />
+            <p>Password</p>
+            <input type="search" />
+
+        </div>
+        
+        </>
+    )
+}
